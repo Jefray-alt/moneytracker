f6f87dff3339844cf3f3d589b2b9b4bef333d783
Jeffrey France <jbfrance1626@gmail.com>
2022-08-29 19:44:07 +0800

    change vant language to english

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import './css/index.css';
 import router from './router/index.js';
+import { Locale } from 'vant';
+import enUS from 'vant/es/locale/lang/en-US';
+
+Locale.use('en-US', enUS);
 
 const app = createApp(App);
 
